477486c03ffeb93867f9eb7cabe42df236c4a1f6
kui <thebazz.98@gmail.com>
2019-12-13 10:01:58 +0200

    fixed styling to airbnb's style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
